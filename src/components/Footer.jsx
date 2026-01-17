fc1cbc587d2b8f066d16cc7bf05a87b699a2cfc3
DavidReds7 <20233tn152@utez.edu.mx>
2026-01-17 01:34:28 -0600

    FEAT: Producto Personalizado y Propinas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(){
       <div className="container d-flex justify-content-between align-items-center">
         <small className="text-muted">Zona 2 &copy; {new Date().getFullYear()} · Coffee Recovery</small>
         <div>
-          <a href="https://prothec.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-muted">Desarrollado por Prothec</a>
+          <a href="https://prothec.vercel.app/" target="_blank" rel="noopener noreferrer" className="underline">Desarrollado por Prothec</a>
         </div>
       </div>
     </footer>
